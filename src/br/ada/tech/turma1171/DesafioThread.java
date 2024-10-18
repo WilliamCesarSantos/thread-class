06cbf9346dcc1d7cb19c3a29478c123c8eab9d95
William Cesar Santos <william_cesar_santos@hotmail.com>
2024-10-18 19:37:54 -0300

    Update

---
+++ src/br/ada/tech/turma1171/DesafioThread.java
@@ -0,0 +1,17 @@
+package br.ada.tech.turma1171;
+
+public class DesafioThread {
+
+    public static void main(String[] args) {
+//        Exercício com arquivos de mock.
+//          - Criar uma forma de pesquisar uma palavra nos arquivos em paralelo
+//          - Pesquisar qualquer parte do texto
+//          - Lembre-se de que são 5 arquivos
+//          - Deve juntar os resultados em uma lista mostrando em qual arquivo estava.
+//             - Vicent
+//                - 5,Vincent,Eason,veason4@feedburner.com -> MOCK_DATA_I.csv
+//                - 906,Vincent,jose,jose@feedburner.com -> MOCK_DATA_I.csv
+//                - 501,Vincent,Jhonson,jhonson@joao.com -> MOCK_DATA_IV.csv
+
+    }
+}
